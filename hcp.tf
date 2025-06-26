6d90a38186d4c74eb98887af92d822bc10624a38
Trey <treyoung12@yahoo.com>
2025-06-26 17:53:56 -0400

    fix underscores

--- hcp.tf
+++ hcp.tf
@@ -5,11 +5,11 @@ resource "hcp_vault_secrets_app" "new-test-app" {
 
 resource "hcp_vault_secrets_secret" "test-user-credential-access-key" {
   app_name     = hcp_vault_secrets_app.new-test-app.app_name
-  secret_name  = "test-access-key"
+  secret_name  = "test_access_key"
   secret_value = aws_iam_access_key.test-user-key.id
 }
 resource "hcp_vault_secrets_secret" "test-user-credential-secret-key" {
   app_name     = hcp_vault_secrets_app.new-test-app.app_name
-  secret_name  = "test-secret-key"
+  secret_name  = "test_secret_key"
   secret_value = aws_iam_access_key.test-user-key.secret
 }
